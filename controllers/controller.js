279521faa5a5e06b637c52c7e5138012e57e8433
Jason Christian Gulla <jasongulla21@gmail.com>
2025-07-29 17:43:41 +0700

    Feat: skeleton done

---
+++ controllers/controller.js
@@ -0,0 +1,24 @@
+const {Op} = require('sequelize');
+const sequelize = require('sequelize');
+
+class Controller{
+    static async x(req, res){
+        try {
+            res.send(error) 
+        } catch (error) {
+            console.log(error);
+            res.send(error) 
+        }
+    };
+    
+     static async x(req, res){
+        try {
+            res.send(error) 
+        } catch (error) {
+            console.log(error);
+            res.send(error) 
+        }
+    };
+}
+
+module.exports =Controller
