922a97ec4013331b7a47d5297e6ba5779fda60ce
stephenJoshua <149865909+stephen1234567890000000@users.noreply.github.com>
2025-07-31 12:54:36 +0700

    controller n route

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,7 +1,43 @@
 class Controller {
-    static x() {
-
+  static home(req, res) {
+    try {
+    } catch (error) {
+      console.log(error);
+      res.send(error);
     }
+  }
+
+  static listAllProducts(req, res) {
+    try {
+    } catch (error) {
+      console.log(error);
+      req.send(error);
+    }
+  }
+
+  static addProductsItems(req, res) {
+    try {
+    } catch (error) {
+      console.log(error);
+      req.send(error);
+    }
+  }
+
+  static saveProductsItems(req, res) {
+    try {
+    } catch (error) {
+      console.log(error);
+      req.send(error);
+    }   
+  }
+
+  static detailProduct(req, res) {
+    try {
+    } catch (error) {
+      console.log(error);
+      req.send(error);
+    }
+  }
 }
 
 module.exports = Controller;
--- routers/router.js
+++ routers/router.js
@@ -2,6 +2,10 @@ const express = require('express');
 const Controller = require('../controllers/controller');
 const router = express.Router()
 
-
+router.get('/', Controller.home);
+router.get('/products', Controller.listAllProducts)
+router.get('/products/add', Controller.addProductsItems)
+router.post('/products/add', Controller.saveProductsItems)
+router.get('/products/:id', Controller.detailProduct)
 
 module.exports = router;
